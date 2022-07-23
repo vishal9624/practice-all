cf7d7a731238d58c77c2b5721a549238e92b68f1
vishal9624 <vishal.dharmare24@gmail.com>
2022-07-23 15:58:57 +0530

    change

--- Terrform/vpc.tf
+++ Terrform/vpc.tf
@@ -1,3 +1,23 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.100.0.0/16"
+  tags = {
+    Name = "demo-vpc"
+  }
+}
+
+resource "aws_subnet" "public1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.100.1.0/24"
+}
+resource "aws_subnet" "public2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.100.2.0/24"
+}
+resource "aws_subnet" "private1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.100.11.0/24"
+}
+resource "aws_subnet" "private2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.100.12.0/24"
 }
