19d15ac1393825e546f7583e7cfdb1a7592c859a
vishal9624 <vishal.dharmare24@gmail.com>
2022-07-27 09:08:28 +0530

    df

--- Terrform/vpc.tf
+++ Terrform/vpc.tf
@@ -15,3 +15,30 @@ resource "aws_subnet" "public1" {
     Name = "subnet1"
   }
 }
+
+resource "aws_subnet" "public2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subnet2"
+  }
+}
+
+resource "aws_subnet" "private1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subnet3"
+  }
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subnet4"
+  }
+}
