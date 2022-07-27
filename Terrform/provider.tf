66b67e457ffd2c3b44988547c5d55dba7ef6e1d3
vishal9624 <vishal.dharmare24@gmail.com>
2022-07-27 08:26:53 +0530

    ch

--- Terrform/provider.tf
+++ Terrform/provider.tf
@@ -1,13 +1,12 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.23.0"
+    }
   }
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
-  acl    = "private"
+provider "aws" {
+  # Configuration options
 }
