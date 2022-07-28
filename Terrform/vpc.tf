beed40dc9617b9e8534a1b5f99d0f9b7546cd4d0
vishal9624 <vishal.dharmare24@gmail.com>
2022-07-28 19:47:57 +0530

    sd

--- Terrform/vpc.tf
+++ Terrform/vpc.tf
@@ -77,7 +77,7 @@ resource "aws_route_table" "NAT_route_table" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.nat.id
+    gateway_id = aws_nat_gateway.nat.id
   }
 }
 
