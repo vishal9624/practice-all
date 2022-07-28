a87bc22e4cfa9c3f6d656643aefd16ac5b2003be
vishal9624 <vishal.dharmare24@gmail.com>
2022-07-28 09:55:15 +0530

    change

--- Terrform/vpc.tf
+++ Terrform/vpc.tf
@@ -62,5 +62,39 @@ resource "aws_nat_gateway" "nat" {
   tags = {
     Name = "gw NAT"
   }
+}
 
+resource "aws_route_table" "IG_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+resource "aws_route_table" "NAT_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.nat.id
+  }
+}
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.IG_route_table.id
+}
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.IG_route_table.id
+}
+
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.NAT_route_table.id
+}
+resource "aws_route_table_association" "private2" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.NAT_route_table.id
 }
