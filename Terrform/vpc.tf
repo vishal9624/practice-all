3dc59c34eb4602ce46d622a9eb031a6d3bbd7863
vishal9624 <vishal.dharmare24@gmail.com>
2022-07-28 09:44:18 +0530

    cf

--- Terrform/vpc.tf
+++ Terrform/vpc.tf
@@ -42,3 +42,25 @@ resource "aws_subnet" "private2" {
     Name = "subnet4"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_eip" "elastic" {
+  vpc      = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.elastic.id
+  subnet_id     = aws_subnet.public1.id
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+}
