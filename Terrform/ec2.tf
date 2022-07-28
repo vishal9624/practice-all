c60895d027a587e2b9d62ecd1337d82eaddcd650
vishal9624 <vishal.dharmare24@gmail.com>
2022-07-28 20:40:56 +0530

    as

--- Terrform/ec2.tf
+++ Terrform/ec2.tf
@@ -1,9 +1,14 @@
-resource "aws_subnet" "vishal_subnet" {
+resource "aws_subnet" "vishal" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "0.0.0.0/24"
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "for_practice"
   }
+}
+
+resource "aws_instance" "flipkart" {
+  ami           = "ami-0888c389af05d881a" # us-west-2
+  instance_type = "t2.micro"
 }
