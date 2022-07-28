3c63503db5306288119c1b0f63d9d8e5132553bd
vishal9624 <vishal.dharmare24@gmail.com>
2022-07-28 09:57:49 +0530

    changes

--- Terrform/vpc.tf
+++ Terrform/vpc.tf
@@ -82,19 +82,19 @@ resource "aws_route_table" "NAT_route_table" {
 }
 
 resource "aws_route_table_association" "public1" {
-  subnet_id      = aws_subnet.main.id
+  subnet_id      = aws_subnet.public1.id
   route_table_id = aws_route_table.IG_route_table.id
 }
 resource "aws_route_table_association" "public2" {
-  subnet_id      = aws_subnet.main.id
+  subnet_id      = aws_subnet.public2.id
   route_table_id = aws_route_table.IG_route_table.id
 }
 
 resource "aws_route_table_association" "private1" {
-  subnet_id      = aws_subnet.main.id
+  subnet_id      = aws_subnet.private1.id
   route_table_id = aws_route_table.NAT_route_table.id
 }
 resource "aws_route_table_association" "private2" {
-  subnet_id      = aws_subnet.main.id
+  subnet_id      = aws_subnet.private2.id
   route_table_id = aws_route_table.NAT_route_table.id
 }
